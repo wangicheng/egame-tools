7a3d98d669e27b3e91a307d28eaaeb58ac5adc69
wangicheng <30070315+wangicheng@users.noreply.github.com>
2025-01-11 16:06:49 +0800

    add keep_current_page

---
+++ js/keep_current_page.js
@@ -0,0 +1,8 @@
+{
+  const match = document.cookie.match(/chapterId=(\d+)/);
+  if(match) {
+    const chapterId = match[1];
+    const button = document.querySelector(`div#chapter_${chapterId}`);
+    button.click();
+  }
+}
