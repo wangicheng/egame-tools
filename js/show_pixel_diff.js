a21d0c16569be1cac2b35da28cd01a6ef48a87a7
wangicheng <30070315+wangicheng@users.noreply.github.com>
2025-01-12 17:44:54 +0800

    remove show_pixel_diff

--- js/show_pixel_diff.js
+++
@@ -1,9 +0,0 @@
-{
-  const visualization = document.querySelector('#visualization');
-  visualization.style.position = 'relative';
-  const canvas1 = document.createElement('canvas');
-  canvas1.width = 400;
-  canvas1.height = 400;
-  visualization.style.position = 'absolute';
-  visualization.appendChild(canvas1);
-}
