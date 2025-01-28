8b64cde2f40dee4fd9efdbcdb1fa3b11503b5c4c
wangicheng <30070315+wangicheng@users.noreply.github.com>
2025-01-28 17:05:20 +0800

    Feat: Add button to reveal answer in show_maker_blocks_dojo.js
    This commit introduces a button to control the display of the answer in show_maker_blocks_dojo.js.
    
    Previously, the answer was shown immediately. This change allows the user to choose when to reveal the answer by clicking the newly added button.

--- js/show_maker_blocks_dojo.js
+++ js/show_maker_blocks_dojo.js
@@ -1,6 +1,22 @@
-const originalParse = JSON.parse;
-
-JSON.parse = (...args) => {
-  Turtle.level.startBlocks = () => Turtle.level.json.makerBlocks;
-  return originalParse(...args);
+{
+  const observer = new MutationObserver(function (mutationList, observer) {
+    const hintButton = document.querySelector("#hintButton");
+    if(!hintButton) {
+      return;
+    }
+    observer.disconnect();
+    const showMakerBlocksButton = hintButton.cloneNode(false);
+    showMakerBlocksButton.textContent = '答案';
+    showMakerBlocksButton.id = 'showMakerBlocksButton';
+    showMakerBlocksButton.title = '顯示武館的答案。';
+    showMakerBlocksButton.addEventListener('click', () => {
+      const xmlText = Turtle.level.json.makerBlocks;
+      const xmlDom = Blockly.Xml.textToDom(`<xml>${xmlText}</xml>`);
+      const workspace = Blockly.getMainWorkspace();
+      workspace.clear();
+      Blockly.Xml.domToWorkspace(workspace, xmlDom);
+    });
+    hintButton.parentElement.insertAdjacentElement('afterbegin', showMakerBlocksButton);
+  });
+  observer.observe(document.documentElement, { attributes: false, childList: true, subtree: true });
 }
