652dc97baeb3d5872ae3a8da95bf5c08cdcec0b1
wangicheng <30070315+wangicheng@users.noreply.github.com>
2025-02-03 11:10:56 +0800

    refactor: Move hide_all_pass functionality from JS to CSS
    The functionality to hide the 'allPass' elements has been moved from js/hide_all_pass.js to css/hide_all_pass.css for better performance and maintainability.

--- js/hide_all_pass.js
+++
@@ -1,6 +0,0 @@
-{
-  const allPassDiv = document.querySelector('#allPassDiv');
-  const allPass = document.querySelector('.allPass');
-  allPassDiv.parentElement.removeChild(allPassDiv);
-  allPass.parentElement.removeChild(allPass);
-}
